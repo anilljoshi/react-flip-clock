523defad9a73e99eb167551e9094cfb1e15358a6
Anil Joshi <joshianil.site@gmail.com>
2018-04-02 12:54:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import FlipClock from './App.jsx';	
+import FlipClock from './App.jsx';
 
-
-ReactDOM.render(<FlipClock {...props}  />, document.getElementById('app'));
+ReactDOM.render(<FlipClock startText='STARTING IN' endText='ENDING IN' startDate=''  endDate='4/2/2019 06:40 PM'  />, document.getElementById('app'));
